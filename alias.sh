0cbab79edf3b521f99977faca48a129e4e803d46
Arkanon <arkanon@lsd.org.br>
2015-11-16 02:30:45 -0200

    Calculado com bfloat e 50 dígitos até n=3.832.000.000

--- alias.sh
+++ alias.sh
@@ -1,3 +1,11 @@
-alias log="watch \"cat 03-avaliacao_de_pontos-050dig-10000-10000-00050.log | tail -n+4 | tr '|' '\t' | cut -f1-4,6,8,9 | sed -r 's/\t/    /g;s/(0\.36)(..)(..)(...)(...).+/\1 \2 \3 \4 \5.../' | tail -n $((LINES-3))\""
-alias off="setterm --blank force"
-alias  on="setterm --blank poke"
+
+  f=03-avaliacao_de_pontos-050dig-10000-10000-00050.log
+  alias conv="watch -n1 \"( cat $f | sed '/^$/d' | uniq -c -s 87 -w 8; echo; tail -n2 $f | sed 's/^/      /' ) | cut -c-107 | cut -d\| -f1-4,6,8,9 | sed -r 's/\|/   /g;s/(0\.36)(..)(..)(....)/\1 \2 \3 \4/' | tail -n $((LINES-3))\""
+# alias conv="watch -n1 \"cat 03-avaliacao_de_pontos-padrao_de_convergencia.log   | sed '/^$/d' | uniq -c -s103 -w10 | cut -c-125 | tail -n $((LINES-3))\""
+
+  alias  log="watch -n1 \"cat $f | tail -n+4 | tr '|' '\t' | cut -f1-4,6,8,9 | sed -r 's/\t/    /g;s/(0\.36)(..)(..)(....)(...).+/\1 \2 \3 \4 \5.../' | tail -n $((LINES-3))\""
+
+
+  alias  off="setterm --blank force"
+  alias   on="setterm --blank poke"
+
--- compare.sh
+++ compare.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 # Arkanon <arkanon@lsd.org.br>
-# 2015/11/14 (Sat) 00:20:55 BRS
+# 2015/11/14 (Sat) 01:37:36 BRD
 # 2015/11/13 (Sex) 16:03:51 BRD
 # 2015/11/13 (Sex) 10:23:00 BRD
 
 # float usa 16 digitos apos a virgula por default (0.36792408175633207)
 # mas a correção comparada com o uso de bfloat tem se mantido apenas ate 7 digitos
 
+  reffile=03-avaliacao_de_pontos-500dig-10000-10000-00050.log
+
   if (( $# < 2 ))
   then
-    echo "Usage: ${0##*/} <log file> <sigfig size>"
+    echo "Usage: ${0##*/} <log file> <sigfig size> [watch]"
     exit
+  fi
+
+  if [ "$3" = watch ]
+  then
+    watch -n1 "$0 $1 $2"
+    exit 
   fi
 
      file=$1
    sigfig=$2
 
-  reffile=03-avaliacao_de_pontos-500dig-10000-10000-00050.log
     LINES=$(tput lines)
 
   cat $reffile | cut -d\| -f9 | cut -c-$((2+sigfig)) | sed '/^$/d' >| zreference.txt
@@ -27,10 +34,9 @@
 
   diff zcompare1.txt <(head -n $n zreference.txt)
 
-  cat zcompare1.txt | sed -r "s/(0\.36)(..)(..)(.)/\1 \2 \3 \4/" >| zcompare2.txt
+  cat zcompare1.txt | sed -r 's/(0\.36)(..)(..)(...)(...)/\1 \2 \3 \4 \5/' >| zcompare2.txt
 
-  echo -e "\n$n\n"
+  echo -e "$n\n"
   tail -n $((LINES-5)) zcompare2.txt
-  echo
 
 # EOF
